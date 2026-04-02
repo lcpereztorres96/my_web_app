6251f3268d06947b62de5a9606b32afc50d8ce49
lcpereztorres96 <tuprofeonline64@gmail.com>
2026-04-02 16:57:59 -0500

    Añadir formulario de contacto

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.getElementById('btnClick').addEventListener('click', function () {
 });
 
 //Este es un metodo
-// document.getElementById('contactForm').addEventListener('submit', function (event) {
-// 	event.preventDefault();
-// 	var name = document.getElementById('name').value;
-// 	var email = document.getElementById('email').value;
-// 	document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
-// });
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+	event.preventDefault();
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
+});
